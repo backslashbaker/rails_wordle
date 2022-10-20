8f9730bd75ff1d3ff706853d8606b08af6d984de
Derek Baker <derek.baker89@gmail.com>
2022-10-20 10:01:50 +0100

    remove commented out code

--- lib/words.rb
+++ lib/words.rb
@@ -1,9 +1,4 @@
 class Words 
-    # WORDS = {
-    #     5 => %w[About Alert Argue Beach Above Found Guess Doubt Every Frame Guest Dozen Exact Frank Guide Draft Exist Fraud Happy Drama Extra Fresh Harry Drawn Faith Front Heart Dream],
-    #     3 => %w[And	Fix	Own Are	Fly	Odd Ape	Fry	Our Ace	For	Pet Act	Got	Pat Ask	Get	Peg Arm	God	Paw Age	Gel	Pup Ago	Gas	Pit Air	Hat	Put Ate	Hit	Pot All	Has	Pop But	Had	Pin Bye	How	Rat Bad	Her	Rag Big	His	Rub Bed	Hen	Row Bat	Ink	Rug Boy	Ice	Run Bus	Ill	Rap Bag	Jab	Ram Box	Jug	Sow],
-    #     10 => %w[Agricolist Alienation Deputation Exultation Aborigines Feathering Likelihood Engraining Decumbency Employment Hydriodate]
-    # }.freeze
     
     def self.get(word_length = 5)
         response = Excon.get("https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=#{word_length}&maxLength=#{word_length}&api_key=#{ENV['API_KEY']}")
@@ -11,6 +6,5 @@ class Words
 
         word = JSON.parse(response.body)
         word["word"].downcase
-        # WORDS[word_length].sample.downcase
     end
 end
